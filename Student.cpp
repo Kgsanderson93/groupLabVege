c7e250787313459553394fdca62a2527abe9d5d9
David Shostak <dpshostak@my.waketech.edu>
2020-12-04 20:08:17 -0500

    setw take 2

--- Student.cpp
+++ Student.cpp
@@ -276,10 +276,10 @@ void Student::calcAvg()
 
 void Student::displayStudent()
 {
-    cout << studentFirstName << " " << studentLastName << setfill (' ') << setw (10) << ssn;
+    cout << studentFirstName << " " << studentLastName << setfill (' ') << setw (12) << ssn;
     for (int i = 0; i < 4; ++i) {
         cout << setw(10) << examGrades[i];
     }
-    cout << setw(10) << examAvg;
+    cout << setw(5) << examAvg;
     cout << endl;
 }
